6896b70a056e08539c0a035fe4c4ce3705ac76ef
luzerz <amonchaibeer@gmail.com>
2020-12-23 16:58:17 +0700

    Add Cors

--- app/app.js
+++ app/app.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const { ThaiCardReader, EVENTS, MODE } = require('./smartcard-reader')
+const cors = require('cors')
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-app.route('').get( (req, res) => {
-  res.send('RESTful API ARE Running');
+app.use(cors());
+app.use(function (req, res, next) {
+    console.log('Time: %d IP:%s => req := %s', Date.now(),req.ip,req.params)
+    next()
+})
+app.route('').get((req, res) => {
+  res.send('RESTFUL API ARE Running');
 });
 
-app.route('/getdata').get( (req,res) => {
+app.route('/getdata').get((req ,res) => {
     const reader = new ThaiCardReader()
     reader.readMode = MODE.PERSONAL_PHOTO
-    reader.autoRecreate = true
+    reader.autoRecreate = false
     reader.startListener()
+    reader.on(EVENTS.CARD_REMOVED,()=>{
+        console.log("Card Removed")
+    })
+    reader.on(EVENTS.DEVICE_DISCONNECTED,()=>{
+        console.log("DEVICE_DISCONNECTED")
+        res.send("DEVICE_DISCONNECTED")
+    })
+    reader.on(EVENTS.PCSC_CLOSE, ()=>{
+        console.log("PCSC_CLOSE")
+        let data = {
+            "ERROR":"PCSC_CLOSE"
+        }
+        res.send(data)
+    })
     reader.on(EVENTS.READING_PROGRESS, (obj) => {
         console.log(obj)
     })
     reader.on(EVENTS.READING_COMPLETE, (obj) => {
         res.send(obj)
     })
-    reader.on(EVENTS.READING_FAIL,(obj) => {
-        let data = {
-            "Eror":"Can't Read Data"
-        }
-        res.send(data)
+    reader.on(EVENTS.READING_FAIL,(err) => {
+        console.log("Error",err)
     })
     
 })
